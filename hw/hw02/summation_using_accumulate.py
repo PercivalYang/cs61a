5d605d9b88c3141b10017e3e5b11292914d7cf93
PercivalYang <yzy1998719@163.com>
2022-04-16 11:31:30 +0800

    add hw02

---
+++ hw/hw02/summation_using_accumulate.py
@@ -0,0 +1,17 @@
+def summation_using_accumulate(n, f):
+    """Returns the sum of f(1) + ... + f(n). The implementation
+    uses accumulate.
+
+    >>> summation_using_accumulate(5, square)
+    55
+    >>> summation_using_accumulate(5, triple)
+    45
+    >>> from construct_check import check
+    >>> # ban iteration and recursion
+    >>> check(HW_SOURCE_FILE, 'summation_using_accumulate',
+    ...       ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+
+
