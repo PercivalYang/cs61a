d2247db43c1c6532891c1d67fa3d843c5e5a251e
PercivalYang <yzy1998719@163.com>
2022-05-11 10:48:24 +0800

    add lab04_2019 & count_coins.py

---
+++ hw/hw03/count_coins.py
@@ -0,0 +1,61 @@
+def get_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> get_larger_coin(1)
+    5
+    >>> get_larger_coin(5)
+    10
+    >>> get_larger_coin(10)
+    25
+    >>> get_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def get_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> get_smaller_coin(25)
+    10
+    >>> get_smaller_coin(10)
+    5
+    >>> get_smaller_coin(5)
+    1
+    >>> get_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+def count_coins(change):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    """
+    "*** YOUR CODE HERE ***"
+    def func(change, largest):
+        # base case
+        if change == 0:
+            return 1
+        if largest == None:
+            return 0
+        if change < 0:
+            return 0
+        with_coin = func(change, get_smaller_coin(largest))
+        without_coin = func(change-largest, largest)
+        return with_coin + without_coin
+    return func(change, 25)
