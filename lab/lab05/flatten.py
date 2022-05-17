3735983ef40c1c10592612ba370984b1f9b483a5
PercivalYang <yzy1998719@163.com>
2022-05-17 09:28:33 +0800

    finish lab05

---
+++ lab/lab05/flatten.py
@@ -0,0 +1,24 @@
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
+    """
+    "*** YOUR CODE HERE ***"
+    f_s = []
+    for i in range(len(s)):
+        if type(s[i]) == list:
+            f_s += flatten(s[i])
+        else:
+            f_s += [s[i]]
+    return f_s
